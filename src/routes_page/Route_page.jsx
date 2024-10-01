511e0d7d70450ea3fb16282eb1078ef3d82b45b6
Abdulquadri-Mahmud <abdulquadrimahmud06@gmail.com>
2024-10-01 08:01:26 +0100

    changes made

--- src/routes_page/Route_page.jsx
+++ src/routes_page/Route_page.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {BrowserRouter as Router, Routes, Route} from 'react-router-dom';
 import Home from '../home/Home';
 import Header from '../components/Header';
-import Social_medial from '../components/fixed_social_media/social_medial';
+import Social_medial from '../components/fixed_social_media/Social_medial';
 
 export default function Route_page() {
   return (
